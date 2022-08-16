82c46ea967bef3f0db016712958e662d5bccf939
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-08-16 21:12:39 +0700

    [UPDATE] section skill implement API

--- sections/home/SectionSkills.jsx
+++ sections/home/SectionSkills.jsx
@@ -1,37 +1,30 @@
 import FancyButton from '../../components/Button/FancyButton';
 import Card from '../../components/Card/Card';
-import UsersIcon from '../../public/images/UsersIcon';
-import UserssIcon from '../../public/images/UserssIcon';
 
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+import { BiLoaderAlt } from 'react-icons/bi';
 
 export default function SectionSkills() {
-    let dummySKill = [
-        {
-            title: 'Python',
-            desc: 'Members, Friends Connection ( like followers ), Private Message',
-        },
-        {
-            title: 'Python',
-            desc: 'Members, Friends Connection ( like followers ), Private Message',
-        },
-        {
-            title: 'Python',
-            desc: 'Members, Friends Connection ( like followers ), Private Message',
-        },
-        {
-            title: 'Python',
-            desc: 'Members, Friends Connection ( like followers ), Private Message',
-        },
-        {
-            title: 'Python',
-            desc: 'Members, Friends Connection ( like followers ), Private Message',
-        },
-        {
-            title: 'Python',
-            desc: 'Members, Friends Connection ( like followers ), Private Message',
-        },
-    ];
+    const [skills, setSkills] = useState([]);
+    const [page, setPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        async function getApiSkill() {
+            setIsLoading(true);
+            const dummySKillRes = await fetch(
+                `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=6`,
+            );
+            setSkills([...skills, ...(await dummySKillRes.json())]);
+            setIsLoading(false);
+        }
+        getApiSkill();
+    }, [page]);
+
+    console.log(skills);
+
     return (
         <section className="py-20 bg-linear-green-center">
             <div className="container mx-auto">
@@ -46,7 +39,7 @@ export default function SectionSkills() {
                     </p>
                 </div>
                 <div className="grid grid-cols-12 gap-8 lg:mt-16">
-                    {dummySKill.map((skill, i) => (
+                    {skills.map((skill, i) => (
                         <Card key={i} className="col-span-6 bg-white">
                             <figure className="flex items-center gap-4">
                                 <div
@@ -56,23 +49,29 @@ export default function SectionSkills() {
                                     <Image
                                         src="/images/icon/tools/1.svg"
                                         height={34}
+                                        width={34}
                                         alt="img"
                                     />
                                 </div>
-                                <figcaption>
+                                <figcaption className="flex-1">
                                     <span className="block text-purple-bold font-bold">
                                         {skill.title}
                                     </span>
                                     <small className="text-gray-helper">
-                                        {skill.desc}
+                                        {skill.body}
                                     </small>
                                 </figcaption>
                             </figure>
                         </Card>
                     ))}
                 </div>
-                <FancyButton className="mt-10 mx-auto">
-                    Discover More
+
+                <FancyButton
+                    className="mt-10 mx-auto gap-2 flex"
+                    onClick={() => setPage(page + 1)}
+                >
+                    <span>Discover More</span>
+                    {isLoading && <BiLoaderAlt className="animate-spin" />}
                 </FancyButton>
             </div>
         </section>
