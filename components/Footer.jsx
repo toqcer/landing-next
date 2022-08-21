ffedfd9763f6788e59947ea61c1f19cbe327c47d
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-08-22 00:13:11 +0700

    [UPDATE] footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,47 @@
-export default function Footer() {
-  return (
-    <footer className="bg-cyan-200 h-20">
-      <div></div>
-      <div className="flex items-center">
-        <div className="">Footer</div>
-      </div>
-    </footer>
-  );
+import Image from 'next/image';
+import Link from 'next/link';
+import { BsInstagram, BsLinkedin, BsWhatsapp } from 'react-icons/bs';
+
+export default function Footer({ className }) {
+    return (
+        <footer
+            className={`bg-cyan-200 h-20 ${className} text-white bg-midnight-blue`}
+        >
+            <div className="container flex justify-between items-center h-full">
+                <div className="flex items-center gap-3">
+                    <div className="relative">
+                        <Image
+                            src="/images/logo-white.png"
+                            height={22}
+                            layout="fill"
+                        />
+                    </div>
+                    <small className="text-sm">Toqcer Digital Agency.</small>
+                </div>
+
+                <div className="flex items-center gap-3">
+                    <Link href="wa.me">
+                        <a className="bg-white rounded-full p-2">
+                            <BsWhatsapp size={20} color="#0E0E38" />
+                        </a>
+                    </Link>
+                    <Link href="wa.me">
+                        <a className="bg-white rounded-full p-2">
+                            <BsWhatsapp size={20} color="#0E0E38" />
+                        </a>
+                    </Link>
+                    <Link href="wa.me">
+                        <a className="bg-white rounded-full p-2">
+                            <BsInstagram size={20} color="#0E0E38" />
+                        </a>
+                    </Link>
+                    <Link href="wa.me">
+                        <a className="bg-white rounded-full p-2">
+                            <BsLinkedin size={20} color="#0E0E38" />
+                        </a>
+                    </Link>
+                </div>
+            </div>
+        </footer>
+    );
 }
