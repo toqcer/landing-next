02d7f98bf8018cbfcda880b6fe03367ad8365624
BariqDharmawan <dharmawan.muhammad03@gmail.com>
2022-09-14 00:04:13 +0700

    [UPDATE] sok sok an typescript

---
+++ config.ts
@@ -0,0 +1,36 @@
+export const FIX_HEIGHT_NAVBAR: number = 85;
+
+export const routes: Array<{ title: string; route: string }> = [
+    {
+        title: 'home',
+        route: '/',
+    },
+    {
+        title: 'services',
+        route: '/services',
+    },
+    {
+        title: 'product',
+        route: '/product',
+    },
+    {
+        title: 'portofolio',
+        route: '/portofolio',
+    },
+    {
+        title: 'client',
+        route: '/client',
+    },
+    {
+        title: 'about',
+        route: '/about',
+    },
+    {
+        title: 'Teams',
+        route: '/teams',
+    },
+    {
+        title: 'contact us',
+        route: '/contact-us',
+    },
+];
